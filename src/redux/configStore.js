e447027590cd0df5aa7455ed056ffb59ab9972c8
user <user@gmail.com>
2020-05-03 11:19:30 +0530

    combined reducer added !

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,7 +1,14 @@
-import { createStore } from "redux";
-import { Reducer, INITIAL_STATE } from "./reducer";
+import { createStore, combineReducers } from "redux";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
 
 export const store = createStore(
-  Reducer, // reducer
-  INITIAL_STATE // our initialState
+  combineReducers({
+    dishes: Dishes,
+    comments: Comments,
+    promotions: Promotions,
+    leaders: Leaders,
+  })
 );
